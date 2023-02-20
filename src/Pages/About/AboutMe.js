ffb04d6137ce19c23e13260135f04e141b8b338d
Everton Benati <eaab2012@gmail.com>
2023-02-20 11:24:13 -0300

    tech added and homepage pic changes

--- src/Pages/About/AboutMe.js
+++ src/Pages/About/AboutMe.js
@@ -1,34 +1,38 @@
-import React from 'react'
-import './AboutMe.css'
+import React from "react";
+import "./AboutMe.css";
 
 function AboutMe() {
   return (
-    <div className="aboutme-section">        
-        <div className='aboutme-text'>
+    <div className="aboutme-section">
+      <div className="aboutme-text">
         <h1>Sobre Mim</h1>
         <p>
-            Sempre fui um entusiasta da programação, mas nunca havia pensado em <br />
-            ser um profissional da área, e há quase um ano passei a me <br />
-            dedicar ao aprendizado de modo a me tornar um programador front-end.
-        </p> 
+          Sempre fui um entusiasta da programação, mas nunca havia pensado em{" "}
+          <br />
+          ser um profissional da área, e há quase um ano passei a me dedicar
+          <br />
+          ao aprendizado de modo a me tornar um programador front-end.
+        </p>
         <p>
-            Sempre fui autodidata, então comecei fazendo o curso CS50 da Harvard,<br />
-            depois passei para o curso completo de Web Developer da Mozilla,<br />
-            em seguida fiz alguns cursos de JavaScript e ReactJS da plataformas <br />
-            Udemy, Scrimba e DIO, e li bons livros de programação.
+          Sempre fui autodidata, então comecei fazendo o curso CS50 da Harvard,
+          <br />
+          depois passei para o curso completo de Web Developer da Mozilla,
+          <br />
+          em seguida fiz alguns cursos de JavaScript e ReactJS da plataformas{" "}
+          <br />
+          Udemy, Scrimba e DIO, e li bons livros de programação.
         </p>
 
         <p>
-            Estou feliz com tudo que aprendi, tenho aprendido e <br />
-            ainda vou aprender. Mais satisfatório ainda é ver todas as <br />
-            coisas que já criei e ainda vou poder criar, pois parar de <br />
-            aprender nunca será uma opção para mim!
+          Estou feliz com tudo que aprendi, tenho aprendido e <br />
+          ainda vou aprender. Mais satisfatório ainda é ver todas as <br />
+          coisas que já criei e ainda vou poder criar, pois parar de <br />
+          aprender nunca será uma opção para mim!
         </p>
-        </div>
-        <div className='aboutme-image'>
-        </div>
+      </div>
+      <div className="aboutme-image"></div>
     </div>
-  )
+  );
 }
 
-export default AboutMe
+export default AboutMe;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import './Home.css'
-import Avatar from '../../images/AvatarEverton.png'
+import React from "react";
+import "./Home.css";
+import Avatar from "../../images/profile-pic-portfolio.png";
 function Home() {
   return (
-    <main className='homepage-section'>               
-        <div className='text-section'>
-            <h3>Olá! Me chamo <br />
-            <span className='name'>Everton BENATI,</span><br />
-              e transformar idéias em código <br />
-              é o que AMO FAZER.
-            </h3>            
-        </div>
-        <div className='avatar'>
-          <img src={Avatar} alt="avatar" />
-        </div>
-    </main>     
-  )
+    <main className="homepage-section">
+      <div className="text-section">
+        <h3>
+          Olá! Me chamo <br />
+          <span className="name">Everton BENATI,</span>
+          <br />
+          e transformar idéias em código <br />é o que AMO FAZER.
+        </h3>
+      </div>
+      <div className="avatar">
+        <img src={Avatar} alt="avatar" />
+      </div>
+    </main>
+  );
 }
 
-export default Home
+export default Home;
