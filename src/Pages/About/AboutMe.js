ef368a223e0498864e3b4448f69d13318fbc23ab
Everton Benati <eaab2012@gmail.com>
2023-02-20 12:41:06 -0300

    aboutme page fix

--- src/Pages/About/AboutMe.js
+++ src/Pages/About/AboutMe.js
@@ -8,27 +8,23 @@ function AboutMe() {
         <h1>Sobre Mim</h1>
         <p>
           Estou cursando Análise e Desenvolvimento de Sistemas, mas sempre fui
-          um entusiasta da tecnologia,
-          <br />
-          portanto bem antes de começar a faculdade já me dedicava <br />
-          ao aprendizado da área, de modo a me tornar um programador front-end.
+          um entusiasta da tecnologia, portanto bem antes de começar a faculdade
+          já me dedicava ao aprendizado da área, de modo a me tornar um
+          programador front-end.
         </p>
         <p>
           Como sempre fui autodidata, comecei fazendo o curso CS50,
-          disponibilizado pela Harvard,
-          <br />
-          depois passei para o curso completo de Web Developer da Mozilla,
-          <br />
-          em seguida fiz alguns cursos de JavaScript e ReactJS da plataformas{" "}
-          <br />
-          Udemy, Scrimba e DIO, e li bons livros de programação.
+          disponibilizado pela Harvard, depois passei para o curso completo de
+          Web Developer da Mozilla, em seguida fiz alguns cursos de JavaScript e
+          ReactJS da plataformas Udemy, Scrimba e DIO, e li bons livros de
+          programação.
         </p>
 
         <p>
           Estou feliz com tudo que aprendi, tenho aprendido e <br />
-          ainda vou aprender. Mais satisfatório ainda é ver todas as <br />
-          coisas que já criei e ainda vou poder criar, pois parar de <br />
-          aprender nunca será uma opção para mim!
+          ainda vou aprender. Mais satisfatório ainda é ver todas as coisas que
+          já criei e ainda vou poder criar, pois parar de aprender nunca será
+          uma opção para mim!
         </p>
       </div>
       <div className="aboutme-image"></div>
